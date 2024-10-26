508b912fed9943d86b193808fb897d2dc491e5b4
analbant <asutay.nalbant@btc-ag.com.tr$ git config --global user.email asutay.nalbant@btc-ag.com.trgit config --get user.analbantgit config --get user.asutay.nalbant@btc-ag.com.tr>
2024-10-26 23:09:02 +0300

    son düzenlemeler yapıldı

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.querySelector(".change-color").addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  document.querySelector(".color").textContent = randomColor;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById("controls");
+const boxesContainer = document.getElementById("boxes");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let boxSize = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxSize += 10;
+    fragment.appendChild(box);
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
